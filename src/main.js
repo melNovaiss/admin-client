61de29e0b3ff3512038694c12f1d7447c501005b
mel <mellnovais14@gmail.com>
2024-05-02 13:01:22 -0300

    commit

--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.js";
-import bcrypt from "bcryptjs";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 
 // Configuração global do Axios
-axios.defaults.headers.common["Authorization"] =
-  localStorage.getItem("token");
+axios.defaults.headers.common["Authorization"] = localStorage.getItem("token");
 
 axios.interceptors.response.use(
   (response) => {
